167e7a267f527a94b764b4bb472dbb170464b50b
Repressoh <85702320+Repressoh@users.noreply.github.com>
2024-04-06 14:30:20 +0200

    free storage solution and pkg-zone update api

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,36 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/cloudflare-workers'
-import getSHA from './utilities/getSHA'
 import getR2file from './utilities/getR2file'
+import { md5 } from "hono/utils/crypto";
+
 
 const app = new Hono()
 
 app.get('/store.db', async (c) => {
-    const file = await getR2file(c.env, "store.db")
-    return c.text(file)
+    return c.redirect("https://store-cdn.repressoh.it/store.db")
 })
 
 
 app.get('/update/homebrew.elf', async (c) => {
-    return c.redirect("https://github.com/LightningMods/PS4-Store/releases/latest/download/homebrew.elf")
+    return c.redirect("https://api.pkg-zone.com/update/homebrew.elf")
 })
 
 app.get('/update/homebrew.elf.sig', async (c) => {
-    return c.redirect("https://github.com/LightningMods/PS4-Store/releases/latest/download/homebrew.elf.sig")
+    return c.redirect("https://api.pkg-zone.com/update/homebrew.elf.sig")
 })
 
 app.get('/update/remote.md5', async (c) => {
-    return c.redirect("https://github.com/LightningMods/PS4-Store/releases/latest/download/remote.md5")
+    return c.redirect("https://api.pkg-zone.com/update/remote.md5")
 })
 
 app.use('/images/*', serveStatic({ root: './' }))
 
 app.get('/api.php', async (c) => {
-    const hash = await getSHA(c.env, "store.db")
+    const res = await fetch(`https://store-cdn.repressoh.it/store.db`)
+    const file = await res.text()
+
+    const hash = md5(file)
+
     return c.json({"hash": hash})
 })
 

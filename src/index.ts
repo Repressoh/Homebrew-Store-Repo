6c5ffeda58edb36dc4063f9045824b23f6d32b5f
Repressoh <85702320+Repressoh@users.noreply.github.com>
2024-04-06 15:20:26 +0200

    back to github for updates

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/cloudflare-workers'
-import getR2file from './utilities/getR2file'
 import { md5 } from "hono/utils/crypto";
 
 
 const app = new Hono()
 
 app.get('/store.db', async (c) => {
-    return c.redirect("https://store-cdn.repressoh.it/store.db")
+    const res = await fetch(`https://store-cdn.repressoh.it/store.db`)
+    const file = await res.text()
+
+    c.header('Content-Type', 'application/x-sqlite3')
+    return c.newResponse(file)
 })
 
 
 app.get('/update/homebrew.elf', async (c) => {
-    return c.redirect("https://api.pkg-zone.com/update/homebrew.elf")
+    return c.redirect("https://github.com/LightningMods/PS4-Store/releases/latest/download/homebrew.elf")
 })
 
 app.get('/update/homebrew.elf.sig', async (c) => {
-    return c.redirect("https://api.pkg-zone.com/update/homebrew.elf.sig")
+    return c.redirect("https://github.com/LightningMods/PS4-Store/releases/latest/download/homebrew.elf.sig")
 })
 
 app.get('/update/remote.md5', async (c) => {
-    return c.redirect("https://api.pkg-zone.com/update/remote.md5")
+    return c.redirect("https://github.com/LightningMods/PS4-Store/releases/latest/download/remote.md5")
 })
 
 app.use('/images/*', serveStatic({ root: './' }))
@@ -29,8 +32,7 @@ app.get('/api.php', async (c) => {
     const res = await fetch(`https://store-cdn.repressoh.it/store.db`)
     const file = await res.text()
 
-    const hash = md5(file)
-
+    const hash = await md5(file)
     return c.json({"hash": hash})
 })
 

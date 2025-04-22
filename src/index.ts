1720f807e2cbeb2421d6aae0b03e7d778d493884
Alberto <195728585+AlbertoCaserta@users.noreply.github.com>
2025-04-22 17:41:06 +0200

    improved typesafety, better error handling & removed unused example assets

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+type Bindings = {
+    BASELINK: string
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.onError((_err,c) => {
+    return c.json({ error: 'Internal Server Error' }, { status: 500 })
+})
+
+app.notFound((c) => {
+    return c.redirect("https://github.com/Repressoh/Homebrew-Store-Repo")
+})
 
 app.get('/store.db', async (c) => {
 
-    const fileData = await fetch(`https://${c.env?.BASELINK}/store.db`).then(res => res.arrayBuffer())
+    const fileData = await fetch(`https://${c.env.BASELINK}/store.db`).then(res => res.arrayBuffer())
 
     return c.newResponse(fileData, {
         headers: {
@@ -14,31 +26,26 @@ app.get('/store.db', async (c) => {
         })
 })
 
-app.get('/update/homebrew.elf', async (c) => {
+app.get('/update/homebrew.elf', (c) => {
     return c.redirect("https://github.com/LightningMods/PS4-Store/releases/latest/download/homebrew.elf")
 })
 
-app.get('/update/homebrew.elf.sig', async (c) => {
+app.get('/update/homebrew.elf.sig', (c) => {
     return c.redirect("https://github.com/LightningMods/PS4-Store/releases/latest/download/homebrew.elf.sig")
 })
 
-app.get('/update/remote.md5', async (c) => {
+app.get('/update/remote.md5', (c) => {
     return c.redirect("https://github.com/LightningMods/PS4-Store/releases/latest/download/remote.md5")
 })
 
 app.get('/api.php', async (c) => {
-    try {
-        const fileData = await fetch(`https://${c.env?.BASELINK}/store.db`).then(res => res.arrayBuffer())
-        
-        const hashBuffer = await crypto.subtle.digest('md5', fileData)
-        const hashArray = Array.from(new Uint8Array(hashBuffer))
-        const md5 = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('')
+    const fileData = await fetch(`https://${c.env.BASELINK}/store.db`).then(res => res.arrayBuffer())
 
-        return c.json({"hash": md5});
+    const hashBuffer = await crypto.subtle.digest('md5', fileData)
+    const hashArray = Array.from(new Uint8Array(hashBuffer))
+    const md5 = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('')
 
-    } catch (error) {
-        return c.json({ error: error.message }, { status: 500 })
-    }
+    return c.json({"hash": md5});
 });
 
 export default app
